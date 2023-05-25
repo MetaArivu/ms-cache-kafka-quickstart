687f689ed90e654fcccfcbbbae54ed6e182378cf
Araf Karsh Hamid <araf.karsh@metamagic.in>
2023-05-25 10:41:48 +0530

    Minor update

--- src/main/java/io/fusion/air/microservice/adapters/repository/ProductRepository.java
+++ src/main/java/io/fusion/air/microservice/adapters/repository/ProductRepository.java
@@ -65,4 +65,6 @@ public interface ProductRepository extends JpaRepository<ProductEntity, UUID> {
      */
     public List<ProductEntity> findByProductNameContains(String name);
 
+    // Added remark
+
 }
